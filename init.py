3c27177eeda0f3735b3f0e219dd5e8534e580ad2
felipen0gueira <felipeaugustonogueira@hotmail.com>
2022-01-21 23:47:47 +0000

    Update init.py

--- init.py
+++ init.py
@@ -16,32 +16,49 @@ waitingInput = True
 while waitingInput:
     waitingInput = input("Seconds:")
     print("nx.tilt_stick(controller_index, Sticks.LEFT_STICK, -100, 0, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, -100, 0, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, -100, 0, tilted=1.5,  released=0.1, block=False)
+
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, up=0.1, block=False)
 
     print("nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 100, 0, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 100, 0, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 100, 0, tilted=1.5,  released=0.1, block=False)
+    
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, released=0.1, block=False)
+
 
     print("nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, -100, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, -100, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, -100, tilted=1.5,  released=0.1, block=False)
+    
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, released=0.1, block=False)
+
 
     print("nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, 100, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, 100, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, 100, tilted=1.5,  released=0.1, block=False)
+    
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput)
+
     
     print("nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, -100, 0, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, -100, 0, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, -100, 0, tilted=1.5,  released=0.1, block=False)
+    
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, released=0.1, block=False)
+
 
     print("nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 100, 0, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 100, 0, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 100, 0, tilted=1.5,  released=0.1, block=False)
+    
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, released=0.1, block=False)
+
 
     print("nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 0, -100, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 0, -100, tilted=1.5)
+    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 0, -100, tilted=1.5,  released=0.1, block=False)
+    
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, released=0.1, block=False)
+
 
     print("nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 0, 100, tilted=1.5)")
-    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 0, 100, tilted=1.5)
-
-    
-
-    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput)
+    nx.tilt_stick(controller_index, Sticks.RIGHT_STICK, 0, 100, tilted=1.5,  released=0.1, block=False)
+    nx.press_buttons(controller_index, [nxbt.Buttons.B], down=waitingInput, released=0.1, block=False)
 
     if(waitingInput == "exit"):
         waitingInput = False
