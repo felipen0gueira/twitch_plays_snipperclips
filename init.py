bb03127903ea61db39009a23959cb11327bd00ae
felipen0gueira <felipeaugustonogueira@hotmail.com>
2022-01-21 13:50:20 +0000

    pressing buttons
    pressing buttons

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
 import nxbt
+import time
 
 nx = nxbt.Nxbt()
 
@@ -6,3 +7,11 @@ controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
 nx.wait_for_connection(controller_index)
 
 print("Connected")
+
+time.sleep(10)
+
+nx.press_buttons(controller_idx, [nxbt.Buttons.B], down=1.0)
+
+nx.tilt_stick(controller_idx, Sticks.RIGHT_STICK, -100, 0, tilted=2.0)
+
+
