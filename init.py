75be4bb86cf36533d7797e6ce9e01a462f17b197
felipen0gueira <felipeaugustonogueira@hotmail.com>
2022-01-22 11:32:56 +0000

    Update init.py

--- init.py
+++ init.py
@@ -60,6 +60,18 @@ while waitingInput:
     nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, 100, tilted=5,  released=0.1, block=False)
     nx.press_buttons(controller_index, [nxbt.Buttons.B, nxbt.Buttons.L], down=waitingInput, up=0.1, block=False)
 
+    btPack = nx.create_input_packet()
+    lStick = btPack["L_STICK"]
+    lStick["LS_LEFT"] = True
+    lStick["X_VALUE"] = 100
+    btPack["L_STICK"] = lStick
+    btPack["A"] = True
+
+    for x in range(120):
+        nx.set_controller_input(btPack)
+
+
+
     if(waitingInput == "exit"):
         waitingInput = False
 
