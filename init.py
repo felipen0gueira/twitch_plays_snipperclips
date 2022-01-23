f893c3974e42939116e6ec1f5b631e45c4b0cdd6
felipen0gueira <felipeaugustonogueira@hotmail.com>
2022-01-23 00:04:57 +0000

    Update init.py

--- init.py
+++ init.py
@@ -3,12 +3,54 @@ from nxbt import Buttons
 from nxbt import Sticks
 import time
 
+
 nx = nxbt.Nxbt()
 
 controller_index = nx.create_controller(nxbt.PRO_CONTROLLER)
 nx.wait_for_connection(controller_index)
 
 print("Connected")
+
+
+
+def jumpLeft():
+
+    
+        btPack = nx.create_input_packet()
+        lStick = btPack["L_STICK"]
+        lStick["LS_LEFT"] = True
+        lStick["X_VALUE"] = -100
+        btPack["L_STICK"] = lStick
+        btPack["B"] = True
+
+        print(btPack)
+        for x in range(220):
+            nx.set_controller_input( controller_index, btPack)
+
+        btPack = nx.create_input_packet()
+        nx.set_controller_input( controller_index, btPack)
+
+def jumpRight():
+        btPack = nx.create_input_packet()
+        lStick = btPack["L_STICK"]
+        lStick["LS_RIGHT"] = True
+        lStick["X_VALUE"] = 100
+        btPack["L_STICK"] = lStick
+        btPack["B"] = True
+
+        print(btPack)
+        for x in range(220):
+            nx.set_controller_input( controller_index, btPack)
+
+        btPack = nx.create_input_packet()
+        nx.set_controller_input( controller_index, btPack)
+
+def right():
+    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 100, 0, tilted=3)
+
+def left():
+    nx.tilt_stick(controller_index, Sticks.LEFT_STICK, -100, 0, tilted=3)
+
 
 
 waitingInput = True
@@ -60,36 +102,18 @@ while waitingInput:
     #nx.tilt_stick(controller_index, Sticks.LEFT_STICK, 0, 100, tilted=5,  released=0.1, block=False)
     #nx.press_buttons(controller_index, [nxbt.Buttons.B, nxbt.Buttons.L], down=waitingInput, up=0.1, block=False)
     if waitingInput == "1":
-
-        btPack = nx.create_input_packet()
-        lStick = btPack["L_STICK"]
-        lStick["LS_LEFT"] = True
-        lStick["X_VALUE"] = -100
-        btPack["L_STICK"] = lStick
-        btPack["B"] = True
-
-        print(btPack)
-        for x in range(220):
-            nx.set_controller_input( controller_index, btPack)
-
-        btPack = nx.create_input_packet()
-        nx.set_controller_input( controller_index, btPack)
+        jumpLeft()
 
 
     if waitingInput == "2":
-        btPack = nx.create_input_packet()
-        lStick = btPack["L_STICK"]
-        lStick["LS_RIGHT"] = True
-        lStick["X_VALUE"] = 100
-        btPack["L_STICK"] = lStick
-        btPack["B"] = True
+        jumpRight()
 
-        print(btPack)
-        for x in range(220):
-            nx.set_controller_input( controller_index, btPack)
 
-        btPack = nx.create_input_packet()
-        nx.set_controller_input( controller_index, btPack)
+    if waitingInput == "3":
+        right()
+
+    if waitingInput == "4":
+        left()
 
 
 
